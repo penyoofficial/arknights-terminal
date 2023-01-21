6e4d04552e689d79f18f4dd451b4102a416b2ec7
Penyo <penyoofficial@outlook.com>
2023-01-21 11:02:15 +0800

    ETP23022
    - 优化了自动生成页面的过程。
    - 改善样式自适应性。
    - 新增对“磨难”关卡的支持。
    - 更新数据。

--- global-auto.js
+++ global-auto.js
@@ -3,12 +3,14 @@ var onlyDisplayOne = false;
 addMaterial();
 // 添加材料
 function addMaterial() {
-    document.writeln("<div>");
+    let mainContain = document.getElementById("main-contain");
     let ms = getJSONObj("data.json").material;
     Array.from(ms).forEach(m => {
-        document.writeln("<div class=\"material\">");
-        document.writeln("    <div class=\"material-icon\" style=\"background-image: url(" + m.icon + ");\"></div>");
-        document.writeln("    <div class=\"material-name\">" + m.name + "</div>");
+        let material = createElement("div", "material");
+        let materialIcon = stylify(createElement("div", "material-icon"), "background-image: url(" + m.icon + ");");
+        let materialName = createElement("div", "material-name", m.name);
+        material.appendChild(materialIcon);
+        material.appendChild(materialName);
         if (onlyDisplayOne)
             addLevel(m.level[0]);
         else
@@ -16,18 +18,22 @@ function addMaterial() {
                 addLevel(l);
             });
         function addLevel(l) {
-            document.writeln("    <div class=\"material-level\">");
-            document.writeln("        <div class=\"level-cost\">");
-            document.writeln("            <div class=\"cost-title\">COST</div>");
-            document.writeln("            <div class=\"cost\">" + l.cost + "</div>");
-            document.writeln("        </div>");
-            document.writeln("        <div class=\"level-code" + (l.rarity == "" ? "" : (" " + l.rarity)) + "\">" + l.code + "</div>");
-            document.writeln("        <div class=\"level-exceptation\">" + l.exceptation + " C/件</div>");
-            document.writeln("    </div>");
+            let materialLevel = createElement("div", "material-level");
+            let levelCost = createElement("div", "level-cost");
+            let costTitle = createElement("div", "cost-title", "COST");
+            let cost = createElement("div", "cost", l.cost);
+            levelCost.appendChild(costTitle);
+            levelCost.appendChild(cost);
+            let levelCode = createElement("div", "level-code" + (l.attribute == "" ? "" : (" " + l.attribute)), l.code);
+            let levelExceptation = createElement("div", "level-exceptation", l.exceptation + " C/件");
+            materialLevel.appendChild(levelCost);
+            materialLevel.appendChild(levelCode);
+            materialLevel.appendChild(levelExceptation);
+            material.appendChild(materialLevel);
         }
-        document.writeln("</div>");
+        mainContain.appendChild(material);
     });
-    document.writeln("<div style=\"clear: both;\"></div></div>");
+    mainContain.appendChild(stylify(createElement("div"), "clear: both;"));
 }
 
 // 获取Json对象
@@ -37,4 +43,20 @@ function getJSONObj(url) {
         dataType: "json",
         async: false
     }).responseText);
+}
+
+// 创建新的HTML元素
+function createElement(tag, className, innerText) {
+    let e = document.createElement(tag);
+    if (className != undefined)
+        e.setAttribute("class", className);
+    if (innerText != undefined)
+        e.innerText = innerText;
+    return e;
+}
+
+// 风格化
+function stylify(e, cssText) {
+    e.style.cssText += cssText;
+    return e;
 }
